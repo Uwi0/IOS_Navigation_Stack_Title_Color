6659ed9d3005af67e9f21036bfe7e284320c3c65
uwi0 <kakapoweb@gmail.com>
2024-08-28 03:09:20 +0700

    Initial Commit

---
+++ Navigation Stack Title Color/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation Stack Title Color
+//
+//  Created by dwi prasetyo on 28/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation Stack Title Color/Navigation_Stack_Title_ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_Stack_Title_ColorApp.swift
+//  Navigation Stack Title Color
+//
+//  Created by dwi prasetyo on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_Stack_Title_ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
